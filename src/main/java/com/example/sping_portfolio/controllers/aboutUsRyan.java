d232bb9c141d5db8f34abf03ab5eddba635ee4a7
RohanKoshy <rkosh230@gmail.com>
2021-09-03 09:59:39 -0700

    AboutUs Rohan

---
+++ src/main/java/com/example/sping_portfolio/controllers/aboutUsRyan.java
@@ -0,0 +1,15 @@
+package com.example.sping_portfolio.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
+public class aboutUsRyan {
+    @GetMapping("/aboutUsRyan")
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String aboutUsRyan() {
+        return "aboutUsRyan"; // returns HTML VIEW (greeting)
+    }
+}
