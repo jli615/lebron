c9d691b0d1bda7f8696870007427ee778048d0c0
rohan <rohannallap@gmail.com>
2021-09-04 23:38:59 -0700

    Genesis Video

--- src/main/java/com/example/sping_portfolio/controllers/GenisisVideoUnit2.java
+++ src/main/java/com/example/sping_portfolio/controllers/GenisisVideoUnit2.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class rohanJournal {
-    @GetMapping("/rohanjournal")
+public class GenisisVideoUnit2 {
+    @GetMapping("/genisisvidu2")
     // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String aboutUsRohan() {
-        return "rohanjournal"; // returns HTML VIEW (greeting)
+    public String genisisvidu2() {
+        return "genisisvidu2"; // returns HTML VIEW (greeting)
     }
 }
