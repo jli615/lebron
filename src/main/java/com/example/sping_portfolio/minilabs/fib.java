b6b7a3bbaa60e89d3253456cf243042f8d18cbd6
Justin <jlisd04@gmail.com>
2021-09-21 09:46:36 -0700

    fib

---
+++ src/main/java/com/example/sping_portfolio/minilabs/fib.java
@@ -0,0 +1,39 @@
+package com.example.sping_portfolio.minilabs;
+import java.util.Scanner;
+
+public class fib {
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to Fibonacci. How many numbers of the Fibonacci sequence do you need?");
+
+        String fib = input.nextLine();
+
+        int fnum = Integer.parseInt(fib);
+
+        int num1 = 0, num2 = 1;
+
+        int count = 0;
+
+        while (count < fnum) {
+
+            // Print the number
+            System.out.print(num1 + " ");
+
+            // Swap
+            int num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+            count = count + 1;
+        }
+
+        int next = count + 1;
+
+        System.out.print("It took " + count + " iterations of the while loop.");
+        System.out.print("the " + next + " th number of the fibonacci sequence is " + num1);
+
+    }
+}
