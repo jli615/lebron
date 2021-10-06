477bcaf68adc7ca5532aaf6532ea166534bff86d
ryanmgds <72889620+ryanmgds@users.noreply.github.com>
2021-10-06 09:13:33 -0700

    interviews

--- src/main/java/com/example/sping_portfolio/controllers/interviewF.java
+++ src/main/java/com/example/sping_portfolio/controllers/interviewF.java
@@ -1,4 +1,14 @@
 package com.example.sping_portfolio.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class interviewF {
+    @GetMapping("/interviewF")
+    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    public String interviewF() {
+        return "interviewF"; // returns HTML VIEW (greeting)
+    }
+
 }
