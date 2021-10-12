77697c2e12d3f21e7f67acdb2f49f4273734e5ea
rohan <rohannallap@gmail.com>
2021-10-11 18:32:27 -0700

    images

--- src/main/java/com/example/sping_portfolio/controllers/grayscale.java
+++ src/main/java/com/example/sping_portfolio/controllers/grayscale.java
@@ -32,16 +32,49 @@ public class grayscale {
         if (toggle == false) {
 
             String file0 = "/images/lebron.png";
-            lii.add(new ImageInfo(file0, web_server + file0, 12));
+            lii.add(new ImageInfo(file0, web_server + file0, 1));
             lii.get(0).read_image();
 
-            String file1 = "/images/lebron.png";
-            lii.add(new ImageInfo(file1, web_server + file1, 2));
+            String file1 = "/images/akumpo.png";
+            lii.add(new ImageInfo(file1, web_server + file1, 1));
             lii.get(1).read_image();
 
-            String file2 = "/images/lebron.png";
-            lii.add(new ImageInfo(file2, web_server + file2, 7));
+            String file2 = "/images/dwade.png";
+            lii.add(new ImageInfo(file2, web_server + file2, 1));
             lii.get(2).read_image();
+
+            String file3 = "/images/harden.png";
+            lii.add(new ImageInfo(file3, web_server + file3, 7));
+            lii.get(3).read_image();
+
+            String file4 = "/images/kobe.png";
+            lii.add(new ImageInfo(file4, web_server + file4, 7));
+            lii.get(4).read_image();
+
+            String file5 = "/images/kyrie.png";
+            lii.add(new ImageInfo(file5, web_server + file5, 7));
+            lii.get(5).read_image();
+
+            String file6 = "/images/meta.png";
+            lii.add(new ImageInfo(file6, web_server + file6, 7));
+            lii.get(6).read_image();
+
+            String file7 = "/images/VC.png";
+            lii.add(new ImageInfo(file7, web_server + file7, 7));
+            lii.get(7).read_image();
+
+            String file8 = "/images/zoin.png";
+            lii.add(new ImageInfo(file8, web_server + file8, 7));
+            lii.get(8).read_image();
+
+            String file9 = "/images/zo.png";
+            lii.add(new ImageInfo(file9, web_server + file9, 7));
+            lii.get(9).read_image();
+
+            String file10 = "/images/lebron james1 .png";
+            lii.add(new ImageInfo(file10, web_server + file10, 7));
+            lii.get(10).read_image();
+
 
         } else {
             String file0 = "/images/lebron.png";
@@ -49,6 +82,7 @@ public class grayscale {
             String str0 = liig.get(0).grayscale();
             lii.add(new ImageInfo(str0, web_server+file0, 12));
             lii.get(0).read_image();
+
         }
 
         model.addAttribute("lii", lii);
