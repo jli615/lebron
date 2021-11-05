85b48e457bb1325a2f4942eb1be1d1d45c82499f
ryanmgds <72889620+ryanmgds@users.noreply.github.com>
2021-11-05 00:54:52 -0700

    FEATURE 1 WORK

--- src/main/java/com/example/sping_portfolio/controllers/scheduleF.java
+++ src/main/java/com/example/sping_portfolio/controllers/scheduleF.java
@@ -1,4 +1,44 @@
 package com.example.sping_portfolio.controllers;
 
 public class scheduleF {
+    int age;
+    int goals;
+    int gameplayed;
+    String name;
+    String position;
+    int number;
+
+    public scheduleF(int ag, int goa, int gam, String nam, String pos, int num)
+    {
+        age = ag;
+        goals = goa;
+        gameplayed = gam;
+        name = nam;
+        position = pos;
+        number = num;
+    }
+    public void displayAge()
+    {
+        System.out.println(age);
+    }
+    public void displayGoals()
+    {
+        System.out.println(goals);
+    }
+    public void displayGamesPlayed()
+    {
+        System.out.println(gameplayed);
+    }
+    public void displayName()
+    {
+        System.out.println(name);
+    }
+    public void displayPosition()
+    {
+        System.out.println(position);
+    }
+    public void displayNumber()
+    {
+        System.out.println(number);
+    }
 }
